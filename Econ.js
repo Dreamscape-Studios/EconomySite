171ca03e3dfb33dd32c170f51a5dabef0cf7a871
KitChaos <97907903+KitChaos-DreamscapeStudios@users.noreply.github.com>
2023-01-11 08:11:09 -0500

    Added base files

---
+++ Econ.js
@@ -0,0 +1,31 @@
+//Game Classes
+class Resource{
+    constructor(name, price, demand, priceDrift, demandDrift, amtInMarket){
+        this.name = name
+        this.price = price
+        this.demand = demand
+       
+    }
+    DriftPrice(){
+
+    }
+   
+}
+class Market{
+    constructor(resources, companies, products, MinWage, PrefWage, Inflation){
+
+    }
+}
+
+class Company{
+    constructor(name, worth, buildings, employees, expenses){
+
+    }
+}
+//HTML Classes
+class Menu{
+    constructor(elements, isOn){
+
+    }
+}
+
