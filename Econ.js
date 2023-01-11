abe61cd038e363c15b12c6f80ec62fd77fa48b77
KitChaos <97907903+KitChaos-DreamscapeStudios@users.noreply.github.com>
2023-01-11 08:19:24 -0500

    Continued work, added external style sheet

--- Econ.js
+++ Econ.js
@@ -1,3 +1,7 @@
+
+//I'll probably get it working in the Inspector console first, then we can get it paged later.
+
+
 //Game Classes
 class Resource{
     constructor(name, price, demand, priceDrift, demandDrift, amtInMarket){
@@ -13,13 +17,32 @@ class Resource{
 }
 class Market{
     constructor(resources, companies, products, MinWage, PrefWage, Inflation){
-
+        this.MinWage = 5
+        this.PrefWage = 7
+        this.Inflation = 0
+        this.resources = {}
+        this.companies = {}
     }
 }
 
 class Company{
     constructor(name, worth, buildings, employees, expenses){
-
+        this.name = name
+        this.worth = 100
+        this.buildings = {}
+        this.employees = 5
+        this.expenses = 0
+        
+    }
+}
+class Building{
+    constructor(name, cost, resource, EmployeeMin, EmployeeMax, conditions){
+        this.name = name
+        this.cost = cost
+        this.resource = resource
+        this.EmployeeMin = EmployeeMin
+        this.EmployeeMax = EmployeeMax
+        this.conditions = conditions
     }
 }
 //HTML Classes
@@ -28,6 +51,6 @@ class Menu{
         this.elements = elements
         this.isOn = false
     }
-    
+
 }
 
